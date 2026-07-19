a2b1695fde82829f2ce2db5b9532cf97d5911d2f
VIGGY2582 <vigneshwarachakravarthy@gmail.com>
2026-07-19 22:53:12 +0530

    basic router using fast api was tested using dummy value

--- python-service/app/main.py
+++ python-service/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.routes.search import router
 
 app = FastAPI(
     title="Music Streaming Python Service",
     description="Handles YouTube search and streaming using yt-dlp",
     version="1.0.0"
 )
+
+app.include_router(router)
+
 
 @app.get("/")
 def home():
