5337452a05637ff3e622afda2fc8125d8369490b
VIGGY2582 <vigneshwarachakravarthy@gmail.com>
2026-07-20 22:15:13 +0530

    Yt-dlp lib included and basic test completed

--- python-service/app/services/youtube_service.py
+++ python-service/app/services/youtube_service.py
@@ -1,12 +1,35 @@
+from yt_dlp import YoutubeDL
+
+
 class YouTubeService:
 
     def search(self, query: str):
-        return [
-            {
-                "videoId": "abc123",
-                "title": f"{query}",
-                "artist": "Dummy Artist",
-                "thumbnail": "https://dummyimage.com/300x300",
-                "duration": "3:25"
-            }
-        ]
+
+        ydl_opts = {
+            "quiet": True,
+            "extract_flat": True
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+
+            result = ydl.extract_info(
+                f"ytsearch10:{query}",
+                download=False
+            )
+
+            songs = []
+
+            if "entries" not in result:
+                return songs
+
+            for video in result["entries"]:
+
+                songs.append({
+                    "videoId": video.get("id"),
+                    "title": video.get("title"),
+                    "artist": video.get("channel"),
+                    "thumbnail": video.get("thumbnails", [{}])[-1].get("url"),
+                    "duration": video.get("duration")
+                })
+
+            return songs
